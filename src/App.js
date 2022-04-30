a68d4be50137eea59776f29134ed8a2c39e41924
Mohammad Naghibi <mohammadnaghibi94@gmail.com>
2022-04-30 13:12:47 +0430

    add base code of each file

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import react from 'react'
+import Info from './components/Info'
+import About from './components/About'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
 
+export default function App() {
+    <>
+        <Info />
+        <About />
+        <Interests />
+        <Footer />
+    </>
+}
